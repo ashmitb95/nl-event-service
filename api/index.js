4437a8ea5ee6acff590761399cae77c970bd8b7e
ashmitb95 <ashmitbbiswas@gmail.co9m>
2023-07-26 20:40:18 +0530

    update vercel.json

--- api/index.js
+++ api/index.js
@@ -1,9 +1,7 @@
 "use strict";
-var __importDefault =
-  (this && this.__importDefault) ||
-  function (mod) {
-    return mod && mod.__esModule ? mod : { default: mod };
-  };
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const routes_1 = __importDefault(require("./routes"));
@@ -12,5 +10,5 @@ const app = (0, express_1.default)();
 app.use(express_1.default.json());
 app.use(routes_1.default);
 app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+    console.log(`Server is running on http://localhost:${port}`);
 });
